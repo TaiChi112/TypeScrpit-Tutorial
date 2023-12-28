c59f03d719f25875438de32efe3bd3c15d223a94
TaiChi <anothai.0978452316@gmail.com>
2023-12-29 00:54:57 +0700

    create division math

--- src/function/index.ts
+++ src/function/index.ts
@@ -1,16 +1,27 @@
+//! addition
 const addition = (number1:number,number2:number):number=>{
     return number1+number2;
 }
 const resultAddition = addition(10,5)
 console.log(`${resultAddition}`);
 
+//TODO subtraction
 const subtraction = (number1:number,number2:number):number=>{
     return number1-number2;
 }
 const resultSubtraction = subtraction(10,5)
 console.log(`${resultSubtraction}`);
 
+// * multiplication
 const multiplication = (number1:number,number2:number):number => {
     return number1*number2;
 }
-const resultMultiplication = multiplication(10,2)
+const resultMultiplication = multiplication(10,2);
+console.log(`${resultMultiplication}`);
+
+
+const division = (number1:number,number2:number):number => {
+    return number1/number2;
+}
+const resultDivision = division(12,2);
+console.log(`${resultDivision}`);
