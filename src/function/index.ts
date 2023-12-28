27ed21c6c5bdb14c43e7954ca5812d77c83ea690
TaiChi <anothai.0978452316@gmail.com>
2023-12-29 00:43:13 +0700

    create addition function basic

---
+++ src/function/index.ts
@@ -0,0 +1,11 @@
+const addition = (number1:number,number2:number):number=>{
+    return number1+number2;
+}
+const resultAddition = addition(10,5)
+console.log(`${resultAddition}`);
+
+const subtraction = (number1:number,number2:number):number=>{
+    return number1-number2;
+}
+const resultSubtraction = subtraction(10,5)
+console.log(`${resultSubtraction}`);
